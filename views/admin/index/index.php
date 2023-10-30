86a4b49f399aef2b348322b12507b52a5f96fed4
Zirngast Rene <52356917+Slipi85@users.noreply.github.com>
2023-10-30 09:34:20 +0100

    Fixed !!Wichtig!!
    Fixed Modul for Bootstrap5
    
    !!Wcihtig!! PHP erweiterung nötig.
    Auch fehler ist vorhanden https://ibb.co/McwRkKt

--- views/admin/index/index.php
+++ views/admin/index/index.php
@@ -11,11 +11,11 @@ $checkouts = $this->get('checkout');
 <h1><?=$this->getTrans('bookings') ?></h1>
 <form class="form-horizontal" method="POST" action="<?=$this->getUrl(['action' => $this->getRequest()->getActionName()]) ?>">
     <?=$this->getTokenField() ?>
-    <div class="form-group <?=$this->validation()->hasError('name') ? 'has-error' : '' ?>">
-        <label for="name" class="col-lg-2 control-label">
+    <div class="row mb-3 <?=$this->validation()->hasError('name') ? 'has-error' : '' ?>">
+        <label for="name" class="col-xl-2 control-label">
             <?=$this->getTrans('name') ?>
         </label>
-        <div class="col-lg-4">
+        <div class="col-xl-4">
             <input type="text"
                    class="form-control"
                    id="name"
@@ -24,11 +24,11 @@ $checkouts = $this->get('checkout');
                    value="<?=$this->escape($this->originalInput('name')) ?>" />
         </div>
     </div>
-    <div class="form-group <?=$this->validation()->hasError('checkoutdate') ? 'has-error' : '' ?>">
-        <label for="datetime" class="col-lg-2 control-label">
+    <div class="row mb-3 <?=$this->validation()->hasError('checkoutdate') ? 'has-error' : '' ?>">
+        <label for="datetime" class="col-xl-2 control-label">
             <?=$this->getTrans('datetime') ?>
         </label>
-        <div class="col-lg-4">
+        <div class="col-xl-4">
             <input type="text"
                    class="form-control"
                    id="datetime"
@@ -37,11 +37,11 @@ $checkouts = $this->get('checkout');
                    value="<?=($this->get('checkoutdate') != '') ? $this->get('checkoutdate') : '' ?>" />
         </div>
     </div>
-    <div class="form-group <?=$this->validation()->hasError('usage') ? 'has-error' : '' ?>">
-        <label for="usage" class="col-lg-2 control-label">
+    <div class="row mb-3 <?=$this->validation()->hasError('usage') ? 'has-error' : '' ?>">
+        <label for="usage" class="col-xl-2 control-label">
             <?=$this->getTrans('usage') ?>
         </label>
-        <div class="col-lg-4">
+        <div class="col-xl-4">
             <input type="text"
                    class="form-control"
                    id="usage"
@@ -50,93 +50,97 @@ $checkouts = $this->get('checkout');
                    value="<?=$this->escape($this->originalInput('usage')) ?>" />
         </div>
     </div>
-    <div class="form-group <?=$this->validation()->hasError('amount') ? 'has-error' : '' ?>">
-        <label for="amount" class="col-lg-2 control-label">
+    <div class="row mb-3 <?=$this->validation()->hasError('amount') ? 'has-error' : '' ?>">
+        <label for="amount" class="col-xl-2 control-label">
             <?=$this->getTrans('amount') ?>
         </label>
-        <div class="col-lg-4">
+        <div class="col-xl-4">
             <input type="text"
                    class="form-control"
                    id="amount"
                    name="amount"
                    placeholder="<?=$this->getTrans('amount') ?>"
-                   data-content="<?=$this->getTrans('amountinfo') ?>" 
-                   rel="popover" 
-                   data-placement="bottom" 
-                   data-original-title="<?=$this->getTrans('amount') ?>" 
+                   data-content="<?=$this->getTrans('amountinfo') ?>"
+                   rel="popover"
+                   data-placement="bottom"
+                   data-original-title="<?=$this->getTrans('amount') ?>"
                    data-trigger="hover"
                    value="<?=$this->escape($this->originalInput('amount')) ?>" />
         </div>
     </div>
     <br>
     <br>
-    <div class="col-lg-4">
-        <h1><?=$this->getTrans('bankbalance') ?></h1>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <strong>
-                    <?php
-                    if ($this->get('amount') != '') {
-                        echo $this->getTrans('balancetotal'),': ', $this->escape($this->getFormattedCurrency($this->get('amount'), $currency));
-                    } else {
-                        echo $this->getTrans('balancetotal'), ': ', $this->escape($this->getFormattedCurrency(0, $currency)) ;
-                    }
-                    ?>
-                </strong>
-                <br>
-                    <?php
-                    if ($this->get('amountplus') != '') {
-                        echo $this->getTrans('totalpaid'), ': ', $this->escape($this->getFormattedCurrency($this->get('amountplus'), $currency));
-                    } else {
-                        echo $this->getTrans('totalpaid'), ': ', $this->escape($this->getFormattedCurrency(0, $currency)) ;
-                    }
-                    ?>
-                <br>
-                <?php
-                if ($this->get('amountminus') != '') {
-                    echo $this->getTrans('totalpaidout'),': ', $this->escape($this->getFormattedCurrency($this->get('amountminus'), $currency));
-                } else {
-                    echo $this->getTrans('totalpaidout'), ': ', $this->escape($this->getFormattedCurrency(0, $currency)) ;
-                }
-                ?>
-            </div>
-        </div>
+    <div class="row">
+      <div class="col-lg-4">
+          <h1><?=$this->getTrans('bankbalance') ?></h1>
+          <div class="card card-default">
+              <div class="card-body">
+                  <strong>
+                      <?php
+                      if ($this->get('amount') != '') {
+                          echo $this->getTrans('balancetotal'),': ', $this->escape($this->getFormattedCurrency($this->get('amount'), $currency));
+                      } else {
+                          echo $this->getTrans('balancetotal'), ': ', $this->escape($this->getFormattedCurrency(0, $currency)) ;
+                      }
+                      ?>
+                  </strong>
+                  <br>
+                      <?php
+                      if ($this->get('amountplus') != '') {
+                          echo $this->getTrans('totalpaid'), ': ', $this->escape($this->getFormattedCurrency($this->get('amountplus'), $currency));
+                      } else {
+                          echo $this->getTrans('totalpaid'), ': ', $this->escape($this->getFormattedCurrency(0, $currency)) ;
+                      }
+                      ?>
+                  <br>
+                  <?php
+                  if ($this->get('amountminus') != '') {
+                      echo $this->getTrans('totalpaidout'),': ', $this->escape($this->getFormattedCurrency($this->get('amountminus'), $currency));
+                  } else {
+                      echo $this->getTrans('totalpaidout'), ': ', $this->escape($this->getFormattedCurrency(0, $currency)) ;
+                  }
+                  ?>
+              </div>
+          </div>
+      </div>
     </div>
-    <div class="col-lg-12">
-        <h1><?=$this->getTrans('bookedpayments') ?></h1>
-        <table class="table table-hover table-striped">
-            <colgroup>
-                <col class="icon_width">
-                <col class="icon_width">
-                <col class="col-lg-2">
-                <col>
-                <col class="col-lg-2">
-                <col>
-            </colgroup>
-            <thead>
-                <tr>
-                    <th></th>
-                    <th></th>
-                    <th><?=$this->getTrans('datetime') ?></th>
-                    <th><?=$this->getTrans('name') ?></th>
-                    <th><?=$this->getTrans('amount') ?></th> 
-                    <th><?=$this->getTrans('usage') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($checkouts ?? [] as $checkout) : ?>
-                <?php $date = new \Ilch\Date($checkout->getDatetime()); ?>
-                <tr>
-                    <td><?=$this->getEditIcon(['action' => 'treatPayment', 'id' => $this->escape($checkout->getId())]) ?></td>
-                    <td><?=$this->getDeleteIcon(['action' => 'del', 'id' => $this->escape($checkout->getId())]) ?></td>
-                    <td><?=$this->escape($date->format(null, true)) ?></td>
-                    <td><?=$this->escape($checkout->getName()) ?></td>
-                    <td><?=$this->escape($this->getFormattedCurrency($checkout->getAmount(), $currency)) ?></td>
-                    <td><?=$this->escape($checkout->getUsage()) ?></td>
-                </tr>
-            <?php endforeach; ?>
-            </tbody>
-        </table>
+    <div class="row">
+      <div class="col-xl-12">
+          <h1><?=$this->getTrans('bookedpayments') ?></h1>
+          <table class="table table-hover table-striped">
+              <colgroup>
+                  <col class="icon_width">
+                  <col class="icon_width">
+                  <col class="col-xl-2">
+                  <col>
+                  <col class="col-xl-2">
+                  <col>
+              </colgroup>
+              <thead>
+                  <tr>
+                      <th></th>
+                      <th></th>
+                      <th><?=$this->getTrans('datetime') ?></th>
+                      <th><?=$this->getTrans('name') ?></th>
+                      <th><?=$this->getTrans('amount') ?></th>
+                      <th><?=$this->getTrans('usage') ?></th>
+                  </tr>
+              </thead>
+              <tbody>
+              <?php foreach ($checkouts ?? [] as $checkout) : ?>
+                  <?php $date = new \Ilch\Date($checkout->getDatetime()); ?>
+                  <tr>
+                      <td><?=$this->getEditIcon(['action' => 'treatPayment', 'id' => $this->escape($checkout->getId())]) ?></td>
+                      <td><?=$this->getDeleteIcon(['action' => 'del', 'id' => $this->escape($checkout->getId())]) ?></td>
+                      <td><?=$this->escape($date->format(null, true)) ?></td>
+                      <td><?=$this->escape($checkout->getName()) ?></td>
+                      <td><?=$this->escape($this->getFormattedCurrency($checkout->getAmount(), $currency)) ?></td>
+                      <td><?=$this->escape($checkout->getUsage()) ?></td>
+                  </tr>
+              <?php endforeach; ?>
+              </tbody>
+          </table>
+        </div>
     </div>
     <?=$this->getSaveBar($this->getTrans('book')) ?>
 </form>
